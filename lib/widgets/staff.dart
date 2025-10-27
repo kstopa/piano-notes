f4bb5fc8c0e43c744298babccef5ff18a7b0e151
K.Stopa <stopa.krzysztof.k@gmail.com>
2025-10-27 22:39:57 +0100

    feat: draw g clef on staff

--- lib/widgets/staff.dart
+++ lib/widgets/staff.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 /// A widget that draws a musical staff (pentagram) with a note on it
 class MusicalStaff extends StatelessWidget {
@@ -15,9 +16,28 @@ class MusicalStaff extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      size: Size(width, height),
-      painter: StaffPainter(note: note),
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Stack(
+        children: [
+          CustomPaint(
+            size: Size(width, height),
+            painter: StaffPainter(note: note),
+          ),
+          // Position the G clef SVG on the staff
+          Positioned(
+            left: 30,
+            top: height / 3 - 20, // Align with staff
+            child: SvgPicture.asset(
+              'assets/gclef.svg',
+              width: 40,
+              height: 110,
+              colorFilter: const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -28,22 +48,22 @@ class StaffPainter extends CustomPainter {
   StaffPainter({required this.note});
 
   // Map notes to their vertical positions on the staff
-  // Position 0 is the middle line (B in treble clef)
+  // Position 6 is the middle line (G in clef)
   // Positive numbers go down, negative numbers go up
   static const Map<String, int> _notePositions = {
     // Treble clef notes
-    'C': 6,   // Below the staff
-    'D': 5,
-    'E': 4,
-    'F': 3,
-    'G': 2,
-    'A': 1,
-    'B': 0,   // Middle line
-    'C#': 6,
-    'D#': 5,
-    'F#': 3,
-    'G#': 2,
-    'A#': 1,
+    'C': 10,   // Below the staff
+    'D': 9,
+    'E': 8,
+    'F': 7,
+    'G': 6,
+    'A': 5,
+    'B': 4,   // Middle line
+    'C#': 10,
+    'D#': 9,
+    'F#': 7,
+    'G#': 6,
+    'A#': 5,
   };
 
   @override
@@ -72,9 +92,6 @@ class StaffPainter extends CustomPainter {
         paint,
       );
     }
-
-    // Draw treble clef (simplified G clef)
-    _drawTrebleClef(canvas, staffStartX + 10, staffStartY, staffLineSpacing, fillPaint);
 
     // Draw the note
     final notePosition = _notePositions[note] ?? 0;
@@ -144,34 +161,6 @@ class StaffPainter extends CustomPainter {
     if (note.contains('#')) {
       _drawSharp(canvas, noteX - 30, noteY, staffLineSpacing / 4, paint);
     }
-  }
-
-  void _drawTrebleClef(Canvas canvas, double x, double y, double spacing, Paint paint) {
-    // Simplified treble clef shape
-    final path = Path();
-
-    // Start at the second line (G line)
-    final gLineY = y + spacing;
-
-    // Draw a stylized G clef
-    path.moveTo(x, gLineY);
-    path.quadraticBezierTo(
-      x + 10, gLineY - spacing * 2,
-      x, gLineY - spacing,
-    );
-    path.quadraticBezierTo(
-      x - 10, gLineY,
-      x, gLineY + spacing,
-    );
-    path.quadraticBezierTo(
-      x + 15, gLineY + spacing * 2,
-      x + 10, gLineY + spacing * 3,
-    );
-
-    canvas.drawPath(path, paint);
-
-    // Draw the curl at the bottom
-    canvas.drawCircle(Offset(x + 5, gLineY + spacing * 3.5), 3, paint);
   }
 
   void _drawSharp(Canvas canvas, double x, double y, double size, Paint paint) {
